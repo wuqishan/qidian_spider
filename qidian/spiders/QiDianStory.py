786d76e3a91c4d9dcfbb47f158400678c31ddd1f
wells <qishan.wu.1@lexisnexis.com>
2018-10-23 14:31:47 +0800

    refs

---
+++ qidian/spiders/QiDianStory.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QiDianStorySpider(scrapy.Spider):
+    name = 'qidianstory'
+    allowed_domains = ['www.qidian.com']
+    start_urls = ['http://www.qidian.com/']
+
+    def parse(self, response):
+
+        docs = response.xpath('//div[@id="classify-list"]/dl/dd')
+        if len(docs) > 0:
+            for node in docs:
+                item = {}
+                item['href'] = node.xpath('./a/@href').extract_first()
+                item['name'] = node.xpath('./a//span[@class="info"]/i/text()').extract_first()
+                item['number'] = node.xpath('./a//span[@class="info"]/b/text()').extract_first()
+                yield item
